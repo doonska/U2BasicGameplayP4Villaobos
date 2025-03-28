02cbfbe1e1ccafd7648839fd2862a78f1edb0467
doonska <727187@student.dusd.net>
2025-03-28 11:46:16 -0700

    u2
    need to do bonus

--- U2BasicGameplayVillalobosP4/Assets/Scripts/PlayerController.cs
+++ U2BasicGameplayVillalobosP4/Assets/Scripts/PlayerController.cs
@@ -18,18 +18,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Keep the player in bounds
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        if (transform.position.x < xRange)
+        //Keep the player in bounds
+
+
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Launch a projectile from the player
